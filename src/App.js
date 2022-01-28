34d0d5eefb2f30330083df91f9710d5b51249635
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-01-28 12:33:31 -0700

    added tests

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from "./components/Gallery";
+import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
@@ -18,16 +18,19 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav>
+      <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      </Nav>
+      ></Nav>
       <main>
-        <Gallery></Gallery>
-        <About></About>
+        <div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
