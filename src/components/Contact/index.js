0080363291f809fe3e7e3a5de0de304162d4c732
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-01-28 15:15:37 -0700

    tests added but new ones are failing. Will fix later

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,39 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
-
-    
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
+
+
     const { name, email, message } = formState;
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-      }
-      
-    //   console.log(formState);
-    
+
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
-      }
+    }
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage('');
+                }
+            }
+        }
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
+    //   console.log(formState);
+
+
     // JSX
     return (
         <section>
@@ -32,6 +51,11 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
